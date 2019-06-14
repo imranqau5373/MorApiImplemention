ade3a4b80d2412e5ec51c6a82ec6b303d32d7745
imranqau5373 <imranqau5373@gmail.com>
2019-06-14 15:56:08 -0700

    new updates

--- routes/users.js
+++ routes/users.js
@@ -19,23 +19,19 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  console.log('in login method avc',req.body)
   var options = { method: 'POST',
 
   url: 'http://62.138.16.114/billing/api/user_login',
-  qs: { u:req.body.username, p: req.body.password },
+  qs: { u:req.body.username, p: req.body.password},
   headers: 
    { 'cache-control': 'no-cache' } };
 
   request(options, function (error, response, body) {
     parseString(body, function (err, result) {
-      console.log(result.action);
-      console.log(result.action.status[0]);
       if(result.action.status[0] == "ok")
-      res.json(JSON.stringify({status : true ,loginId : result.action.user_id[0]}));
+      res.json({status : true ,loginId : result.action.user_id[0]});
       else
       res.json({status : false});
-
     });
 
 });
