d7a49089f39d878e59cf165e8915562b099812d8
imranqau5373 <imranqau5373@gmail.com>
2019-07-06 11:58:09 +0500

    cross origin request

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use('/users', usersRouter);
 app.use('/sip', sipRouter);
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
